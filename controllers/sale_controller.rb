fc0e592b99d726b8d2b8f58ec398bb4b6aa15bf1
Rodrigo Cavalcanti de Castro <rodrigo.darlane@gmail.com>
2021-11-02 11:10:20 -0300

    Add files via upload

--- controllers/sale_controller.rb
+++ controllers/sale_controller.rb
@@ -6,7 +6,7 @@ class SaleController < ApplicationController
 
     def show
         sale = Sale.find(params[:id])
-        render json: {status: 'SUCCESS', message:'Loaded sale', data:sale},status: :ok
+        render json: {status: 'SUCCESS', message:'Loaded sale', data:[ "sale": sale, "productsales": sale.productsales]},status: :ok
     end
     
     def create
@@ -17,20 +17,8 @@ class SaleController < ApplicationController
             Sale.transaction do
                 sale = Sale.new(sale_params)
                 if sale.save
-                    count = 0
-                    arrProducts = params[:products]
                     sale_id = sale[:id]
-                    arrProducts.each do |product|
-                        newproduct = Hash.new
-                        newproduct[:sale_id] = "#{sale_id}"
-                        newproduct[:product_id] = product[:product_id]
-                        newproduct[:value] = product[:value]
-                        newproduct[:quantity] = product[:quantity]
-                        retprd = Productsale.new(newproduct)
-                        retprd.save
-                        newArrPrd[count] = retprd
-                        count = count + 1
-                    end 
+                    addProductSales(sale_id)
                     ret = true
                 end 
             end
@@ -43,34 +31,84 @@ class SaleController < ApplicationController
         end
 
         if ret==true
-            render json: {status: 'SUCCESS', message:'Saved sale', data:sale},status: :ok
+            render json: {status: 'SUCCESS', message:'Saved sale', data:[ "sale": sale, "productsales": sale.productsales]},status: :ok
         else
             render json: {status: 'ERROR', message:'Sale not saved', data:sale.errors},status: :unprocessable_entity
         end
           
-  end
+    end
 
-  def destroy
-      sale = Sale.find(params[:id])
-      if sale.destroy
-          render json: {status: 'SUCCESS', message:'Deleted sale', data:sale},status: :ok
-      else
-          render json: {status: 'ERROR', message:'sale not deleted', data:sale.errors},status: :unprocessable_entity
-      end
-  end
+    def addProductSales(sale_id)
+        arrProducts = params[:productsales]
+puts "arrProducts=#{arrProducts}"
+        arrProducts.each do |product|
+            newproduct = Hash.new
+            newproduct[:sale_id] = "#{sale_id}"
+            newproduct[:product_id] = product[:product_id]
+            newproduct[:value] = product[:value]
+            newproduct[:quantity] = product[:quantity]
+            retprd = Productsale.new(newproduct)
+            retprd.save
+        end
+    end
 
-  def update
-      sale = Sale.find(params[:id])
-      if sale.update(sale_params)
-          render json: {status: 'SUCCESS', message:'Updated sale', data:sale},status: :ok
-      else
-          render json: {status: 'ERROR', message:'Sale not updated', data:sale.errors},status: :unprocessable_entity
-      end
-  end
+    def delProductSales(sale_id)
+puts "entrou-sale-id: #{sale_id}"
+        Productsale.where(:sale_id => sale_id).delete_all
+    end
+
+    def update
+        ret = false
+        sale = nil
+        begin
+            Sale.transaction do
+                sale_id = params[:id]
+                sale = Sale.find(sale_id)
+                newsale = Hash.new
+                newsale[:id] = sale_id
+                newsale[:client_id] = params[:client_id]
+                newsale[:seller_id] = params[:seller_id]
+                newsale[:created_at] = sale[:created_at]
+                newsale[:updated_at] = sale[:updated_at]
+                if sale.update(newsale)
+                    delProductSales(sale_id)
+                    addProductSales(sale_id)
+                    ret = true
+                end 
+            end
+        rescue StandardError, AnotherError => e
+            ret = false
+            #puts e.inspect
+            raise ActiveRecord::Rollback, e.inspect
+        ensure
+
+        end
+
+        if ret==true
+            render json: {status: 'SUCCESS', message:'Updated sale', data:[ "sale": sale, "productsales": sale.productsales]},status: :ok
+        else
+            render json: {status: 'ERROR', message:'Sale not updated', data:sale.errors},status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        sale_id = params[:id]
+        delProductSales(sale_id)
+        sale = Sale.find(params[:id])
+        if sale.destroy
+            render json: {status: 'SUCCESS', message:'Deleted sale', data:sale},status: :ok
+        else
+            render json: {status: 'ERROR', message:'sale not deleted', data:sale.errors},status: :unprocessable_entity
+        end
+    end
 
     private
 
     def sale_params
-        params.permit(:client_id, :seller_id, :products)
+        params.permit(:client_id, :seller_id, :productsales)
+    end
+
+    def sale_params_update
+        params.permit(:id, :client_id, :seller_id, :productsales)
     end
 end 
